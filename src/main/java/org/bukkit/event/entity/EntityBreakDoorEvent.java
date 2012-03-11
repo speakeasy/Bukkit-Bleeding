713fafb96de6c3543d9c624c4413a8aedef8107a
feildmaster <admin@feildmaster.com>
2012-03-10 21:02:21 -0600

    Extending EntityBlockChangeEvent doesn't sound too bad.

--- src/main/java/org/bukkit/event/entity/EntityBreakDoorEvent.java
+++ src/main/java/org/bukkit/event/entity/EntityBreakDoorEvent.java
@@ -1,48 +1,17 @@
 package org.bukkit.event.entity;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
-import org.bukkit.event.Cancellable;
-import org.bukkit.event.HandlerList;
+import org.bukkit.entity.LivingEntity;
 
 /**
  * Called when an {@link Entity} breaks a door
  * <p />
  * Canceling the event will cause the event to be delayed
  */
-public class EntityBreakDoorEvent extends EntityEvent implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
-    private Block target;
-    private boolean cancelled = false;
-
-    public EntityBreakDoorEvent(final Entity entity, final Block targetBlock) {
-        super(entity);
-        target = targetBlock;
-    }
-
-    /**
-     * Get the {@link Block} the {@link Entity} is targeting
-     *
-     * @return the block
-     */
-    public Block getTargetBlock() {
-        return target;
-    }
-
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    public void setCancelled(boolean cancel) {
-        cancelled = cancel;
-    }
-
-    @Override
-    public HandlerList getHandlers() {
-        return handlers;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlers;
+public class EntityBreakDoorEvent extends EntityChangeBlockEvent {
+    public EntityBreakDoorEvent(final LivingEntity entity, final Block targetBlock) {
+        super(entity, targetBlock, Material.AIR);
     }
 }
