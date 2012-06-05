846575d95352e2031d85c176172bc7e14d26ce0f
Aidan Matzko <amatzko48@gmail.com>
2012-06-05 00:37:21 -0400

    Expose JavaPlugin.getFile Addresses BUKKIT-1761

--- src/main/java/org/bukkit/plugin/java/JavaPlugin.java
+++ src/main/java/org/bukkit/plugin/java/JavaPlugin.java
@@ -93,7 +93,7 @@ public abstract class JavaPlugin extends PluginBase {
      *
      * @return File containing this plugin
      */
-    protected File getFile() {
+    public File getFile() {
         return file;
     }
 
