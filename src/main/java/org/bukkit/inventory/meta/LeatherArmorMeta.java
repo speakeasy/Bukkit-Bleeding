4be44696c26905b39dd7793f70950e382d5c5f85
Ivan Panarusky <eviltechie@joe.to>
2012-12-03 03:06:26 -0500

    Javadocs for ArmorMeta

--- src/main/java/org/bukkit/inventory/meta/LeatherArmorMeta.java
+++ src/main/java/org/bukkit/inventory/meta/LeatherArmorMeta.java
@@ -3,6 +3,18 @@ package org.bukkit.inventory.meta;
 import org.bukkit.Color;
 
 public interface LeatherArmorMeta extends ItemMeta {
+
+    /**
+     * Gets the color of the armor
+     * 
+     * @return the color of the armor
+     */
     Color getColor();
+
+    /**
+     * Sets the color of the armor
+     * 
+     * @param color the color to set
+     */
     void setColor(Color color);
 }
