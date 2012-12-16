e3a0648b151850b1fa5b9127c4bdbc3e96d3d918
Wesley Wolfe <weswolf@aol.com>
2012-12-15 20:52:46 -0600

    I can has null?

--- src/main/java/org/bukkit/inventory/meta/LeatherArmorMeta.java
+++ src/main/java/org/bukkit/inventory/meta/LeatherArmorMeta.java
@@ -11,14 +11,14 @@ public interface LeatherArmorMeta extends ItemMeta {
     /**
      * Gets the color of the armor
      *
-     * @return the color of the armor
+     * @return the color of the armor, never null
      */
     Color getColor();
 
     /**
      * Sets the color of the armor
      *
-     * @param color the color to set
+     * @param color the color to set, null makes it the default leather color
      */
     void setColor(Color color);
 
