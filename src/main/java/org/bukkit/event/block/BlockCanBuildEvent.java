936bcca5f75a304209c0d35d434cc1c3856e4cbe
mbax <github@phozop.net>
2013-01-30 12:33:00 -0500

    Add Entity to BlockCanBuildEvent. Adds BUKKIT-3532
    New getter:
      * getEntity
          Gets the Entity placing the block
          Generally a Player, could be a falling block
          Null if a plugin is using the deprecated method
    
    Also cleaned javadocs

--- src/main/java/org/bukkit/event/block/BlockCanBuildEvent.java
+++ src/main/java/org/bukkit/event/block/BlockCanBuildEvent.java
@@ -2,31 +2,42 @@ package org.bukkit.event.block;
 
 import org.bukkit.block.Block;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.HandlerList;
 
 /**
- * Called when we try to place a block, to see if we can build it here or not.
+ * Called when we try to place a block, to see if we can build it here or not
  * <p>
  * Note:
  * <ul>
- * <li>The Block returned by getBlock() is the block we are trying to place on, not the block we are trying to place.</li>
- * <li>If you want to figure out what is being placed, use {@link #getMaterial()} or {@link #getMaterialId()} instead.</li>
+ * <li>The Block returned by getBlock() is the block we are trying to place
+ * on, not the block we are trying to place.</li>
+ * <li>If you want to figure out what is being placed, use
+ * {@link #getMaterial()} or {@link #getMaterialId()} instead.</li>
  * </ul>
  */
 public class BlockCanBuildEvent extends BlockEvent {
     private static final HandlerList handlers = new HandlerList();
-    protected boolean buildable;
-    protected int material;
+    private boolean buildable;
+    private final int material;
+    private final Entity entity;
 
+    @Deprecated
     public BlockCanBuildEvent(final Block block, final int id, final boolean canBuild) {
+        this(block, id, canBuild, null);
+    }
+
+    public BlockCanBuildEvent(final Block block, final int id, final boolean canBuild, final Entity entity) {
         super(block);
-        buildable = canBuild;
-        material = id;
+        this.buildable = canBuild;
+        this.material = id;
+        this.entity = entity;
     }
 
     /**
      * Gets whether or not the block can be built here.
-     * By default, returns Minecraft's answer on whether the block can be built here or not.
+     * By default, returns Minecraft's answer on whether the block can be
+     * built here or not.
      *
      * @return boolean whether or not the block can be built
      */
@@ -37,10 +48,11 @@ public class BlockCanBuildEvent extends BlockEvent {
     /**
      * Sets whether the block can be built here or not.
      *
-     * @param cancel true if you want to allow the block to be built here despite Minecraft's default behaviour
+     * @param buildable true if you want to allow the block to be built here
+     *                  despite Minecraft's default behaviour
      */
-    public void setBuildable(boolean cancel) {
-        this.buildable = cancel;
+    public void setBuildable(boolean buildable) {
+        this.buildable = buildable;
     }
 
     /**
@@ -59,6 +71,16 @@ public class BlockCanBuildEvent extends BlockEvent {
      */
     public int getMaterialId() {
         return material;
+    }
+
+    /**
+     * Gets the Entity trying to place the block.
+     *
+     * @return The Entity trying to place the block or null if no entity is
+     *         involved.
+     */
+    public Entity getEntity() {
+        return this.entity;
     }
 
     @Override
