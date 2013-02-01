07a01e347d438641584dadb4c210c8da45f35b30
Score_Under <seejay.11@gmail.com>
2013-02-01 09:47:32 +0000

    Add relevant methods to AnvilInventory

--- src/main/java/org/bukkit/inventory/AnvilInventory.java
+++ src/main/java/org/bukkit/inventory/AnvilInventory.java
@@ -1,4 +1,47 @@
 package org.bukkit.inventory;
 
 public interface AnvilInventory extends Inventory {
+    //TODO Are the javadocs OK
+
+    /**
+     * Gets the base item to combine on the anvil.
+     *
+     * @return the item to combine on the anvil
+     */
+    ItemStack getBaseItem();
+
+    /**
+     * Sets the base item to combine on the anvil.
+     *
+     * @param item the new item
+     */
+    void setBaseItem(ItemStack item);
+
+    /**
+     * Gets the item to be combined with on the anvil.
+     *
+     * @return the item to be combined with on the anvil
+     */
+    ItemStack getCombiningItem();
+
+    /**
+     * Sets the item to be combined with on the anvil.
+     *
+     * @param item the new item
+     */
+    void setCombiningItem(ItemStack item);
+
+    /**
+     * Gets the item resulting from the combination of the items on the anvil.
+     *
+     * @return the resultant item
+     */
+    ItemStack getResult();
+
+    /**
+     * Sets the item that will result from the combination of the items on the anvil.
+     *
+     * @param item the new resultant item
+     */
+    void setResult(ItemStack item);
 }
