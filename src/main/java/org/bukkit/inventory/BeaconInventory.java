49fe1383e89d394ab6712ddc2ee5475248b6d444
riking <rikingcoding@gmail.com>
2013-08-13 15:35:41 -0700

    Specify stricter getHolder() in BeaconInventory

--- src/main/java/org/bukkit/inventory/BeaconInventory.java
+++ src/main/java/org/bukkit/inventory/BeaconInventory.java
@@ -1,5 +1,10 @@
 package org.bukkit.inventory;
 
+import org.bukkit.block.Beacon;
+
+/**
+ * Represents a Beacon's inventory.
+ */
 public interface BeaconInventory extends Inventory {
 
     /**
@@ -8,10 +13,18 @@ public interface BeaconInventory extends Inventory {
      * @param item The new item
      */
     void setItem(ItemStack item);
+
     /**
      * Get the item powering the beacon.
      *
      * @return The current item.
      */
     ItemStack getItem();
+
+    /**
+     * Get the Beacon this BeaconInventory belongs to.
+     *
+     * @return the Beacon BlockState
+     */
+    public Beacon getHolder();
 }
