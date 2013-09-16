ebb55e240e08639f41936f861de31e2be2580e5e
GJ <gjmcferrin@gmail.com>
2013-09-16 14:51:13 -0400

    Add getType and setType to SkullMeta

--- src/main/java/org/bukkit/inventory/meta/SkullMeta.java
+++ src/main/java/org/bukkit/inventory/meta/SkullMeta.java
@@ -1,6 +1,7 @@
 package org.bukkit.inventory.meta;
 
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 
 /**
  * Represents a skull ({@link Material#SKULL_ITEM}) that can have an owner.
@@ -10,7 +11,7 @@ public interface SkullMeta extends ItemMeta {
     /**
      * Gets the owner of the skull.
      *
-     * @return the owner if the skull
+     * @return the owner of the skull
      */
     String getOwner();
 
@@ -29,6 +30,20 @@ public interface SkullMeta extends ItemMeta {
      * @return true if the owner was successfully set
      */
     boolean setOwner(String owner);
+
+    /**
+     * Gets the type of the skull.
+     *
+     * @return the type of the skull
+     */
+    SkullType getType();
+
+    /**
+     * Sets the type of the skull.
+     *
+     * @return true if the type of the skull was successfully set
+     */
+    boolean setType(SkullType type);
 
     SkullMeta clone();
 }
