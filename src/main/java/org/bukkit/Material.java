9dc815a7bd525359456a2aa065e4a07d2ba31a3d
GJ <gjmcferrin@gmail.com>
2013-12-03 22:55:08 -0500

    Add new enum & MaterialData for QuartzBlock

--- src/main/java/org/bukkit/Material.java
+++ src/main/java/org/bukkit/Material.java
@@ -38,6 +38,7 @@ import org.bukkit.material.PistonExtensionMaterial;
 import org.bukkit.material.PoweredRail;
 import org.bukkit.material.PressurePlate;
 import org.bukkit.material.Pumpkin;
+import org.bukkit.material.QuartzBlock;
 import org.bukkit.material.Rails;
 import org.bukkit.material.RawFish;
 import org.bukkit.material.RedstoneTorch;
@@ -225,7 +226,7 @@ public enum Material {
     REDSTONE_BLOCK(152),
     QUARTZ_ORE(153),
     HOPPER(154),
-    QUARTZ_BLOCK(155),
+    QUARTZ_BLOCK(155, QuartzBlock.class),
     QUARTZ_STAIRS(156, Stairs.class),
     ACTIVATOR_RAIL(157, PoweredRail.class),
     DROPPER(158, Dispenser.class),
---
+++ src/main/java/org/bukkit/material/QuartzBlock.java
@@ -0,0 +1,143 @@
+package org.bukkit.material;
+
+import org.bukkit.Material;
+import org.bukkit.QuartzType;
+import org.bukkit.block.BlockFace;
+
+public class QuartzBlock extends MaterialData implements Directional {
+    public QuartzBlock() {
+        super(Material.QUARTZ_BLOCK);
+    }
+
+    public QuartzBlock(QuartzType type) {
+        this();
+        setType(type);
+    }
+
+    public QuartzBlock(QuartzType type, BlockFace dir) {
+        this();
+        setType(type);
+        setFacingDirection(dir);
+    }
+
+    /**
+     *
+     * @deprecated Magic value
+     */
+    @Deprecated
+    public QuartzBlock(final int type) {
+        super(type);
+    }
+
+    public QuartzBlock(final Material type) {
+        super(type);
+    }
+
+    /**
+     *
+     * @deprecated Magic value
+     */
+    @Deprecated
+    public QuartzBlock(final int type, final byte data) {
+        super(type, data);
+    }
+
+    /**
+     *
+     * @deprecated Magic value
+     */
+    @Deprecated
+    public QuartzBlock(final Material type, final byte data) {
+        super(type, data);
+    }
+
+    /**
+     * Get the type of this quartz
+     *
+     * @return QuartzType of this quartz
+     */
+    public QuartzType getType() {
+        switch (getData()) {
+            case 0x0:
+                return QuartzType.SMOOTH;
+            case 0x1:
+                return QuartzType.CHISELED;
+            default:
+                return QuartzType.PILLAR;
+        }
+    }
+
+    /**
+     * Set the type of this quartz
+     *
+     * @param type New type of this quartz
+     */
+    public void setType(QuartzType type) {
+        switch (type) {
+            case SMOOTH:
+                setData((byte) 0x0);
+                return;
+            case CHISELED:
+                setData((byte) 0x1);
+                return;
+            case PILLAR:
+                setData((byte) 0x2);
+                return;
+        }
+    }
+
+    /**
+     * Get direction of the quartz
+     *
+     * @return BlockFace.TOP for upright (default), BlockFace.NORTH (east-west), BlockFace.WEST (north-south), BlockFace.SELF (directionless)
+     */
+    @Override
+    public BlockFace getFacing() {
+        switch (getData()) {
+            case 0x2:
+                return BlockFace.UP;
+            case 0x3:
+                return BlockFace.WEST;
+            case 0x4:
+                return BlockFace.NORTH;
+            default:
+                return BlockFace.SELF;
+        }
+    }
+
+    /**
+     * Set the direction of this quartz
+     *
+     * @param dir direction end of quartz (BlockFace.SELF for no direction)
+     */
+    @Override
+    public void setFacingDirection(BlockFace dir) {
+        switch (dir) {
+            case UP:
+            case DOWN:
+                setData((byte) 0x2);
+                return;
+            case WEST:
+            case EAST:
+                setData((byte) 0x3);
+                return;
+            case NORTH:
+            case SOUTH:
+                setData((byte) 0x4);
+                return;
+            default:
+                setData((byte) 0x0);
+                return;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getType() + " " + super.toString() + " facing " + getFacing();
+    }
+
+    @Override
+    public QuartzBlock clone() {
+        return (QuartzBlock) super.clone();
+    }
+}
