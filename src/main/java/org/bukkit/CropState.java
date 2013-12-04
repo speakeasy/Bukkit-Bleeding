1fee1ae591361218c600a850d4df7b6bd7e853de
GJ <gjmcferrin@gmail.com>
2013-12-04 10:10:36 -0500

    [BREAKING] Move Crop data values into MaterialData
    This allows for the removal of the deprecated methods.

--- src/main/java/org/bukkit/CropState.java
+++ src/main/java/org/bukkit/CropState.java
@@ -1,9 +1,5 @@
 package org.bukkit;
 
-import java.util.Map;
-
-import com.google.common.collect.Maps;
-
 /**
  * Represents the different growth states of crops
  */
@@ -12,71 +8,33 @@ public enum CropState {
     /**
      * State when first seeded
      */
-    SEEDED(0x0),
+    SEEDED,
     /**
      * First growth stage
      */
-    GERMINATED(0x1),
+    GERMINATED,
     /**
      * Second growth stage
      */
-    VERY_SMALL(0x2),
+    VERY_SMALL,
     /**
      * Third growth stage
      */
-    SMALL(0x3),
+    SMALL,
     /**
      * Fourth growth stage
      */
-    MEDIUM(0x4),
+    MEDIUM,
     /**
      * Fifth growth stage
      */
-    TALL(0x5),
+    TALL,
     /**
      * Almost ripe stage
      */
-    VERY_TALL(0x6),
+    VERY_TALL,
     /**
      * Ripe stage
      */
-    RIPE(0x7);
-
-    private final byte data;
-    private final static Map<Byte, CropState> BY_DATA = Maps.newHashMap();
-
-    private CropState(final int data) {
-        this.data = (byte) data;
-    }
-
-    /**
-     * Gets the associated data value representing this growth state
-     *
-     * @return A byte containing the data value of this growth state
-     * @deprecated Magic value
-     */
-    @Deprecated
-    public byte getData() {
-        return data;
-    }
-
-    /**
-     * Gets the CropState with the given data value
-     *
-     * @param data
-     *            Data value to fetch
-     * @return The {@link CropState} representing the given value, or null if
-     *         it doesn't exist
-     * @deprecated Magic value
-     */
-    @Deprecated
-    public static CropState getByData(final byte data) {
-        return BY_DATA.get(data);
-    }
-
-    static {
-        for (CropState cropState : values()) {
-            BY_DATA.put(cropState.getData(), cropState);
-        }
-    }
+    RIPE;
 }
--- src/main/java/org/bukkit/material/Crops.java
+++ src/main/java/org/bukkit/material/Crops.java
@@ -53,7 +53,26 @@ public class Crops extends MaterialData {
      * @return CropState of this crop
      */
     public CropState getState() {
-        return CropState.getByData(getData());
+        switch (getData()) {
+            case 0x0:
+                return CropState.SEEDED;
+            case 0x1:
+                return CropState.GERMINATED;
+            case 0x2:
+                return CropState.VERY_SMALL;
+            case 0x3:
+                return CropState.SMALL;
+            case 0x4:
+                return CropState.MEDIUM;
+            case 0x5:
+                return CropState.TALL;
+            case 0x6:
+                return CropState.VERY_TALL;
+            case 0x7:
+                return CropState.RIPE;
+            default:
+                return null;
+        }
     }
 
     /**
@@ -62,7 +81,32 @@ public class Crops extends MaterialData {
      * @param state New growth state of this crop
      */
     public void setState(CropState state) {
-        setData(state.getData());
+        switch (state) {
+            case SEEDED:
+                setData((byte) 0x0);
+                return;
+            case GERMINATED:
+                setData((byte) 0x1);
+                return;
+            case VERY_SMALL:
+                setData((byte) 0x2);
+                return;
+            case SMALL:
+                setData((byte) 0x3);
+                return;
+            case MEDIUM:
+                setData((byte) 0x4);
+                return;
+            case TALL:
+                setData((byte) 0x5);
+                return;
+            case VERY_TALL:
+                setData((byte) 0x6);
+                return;
+            case RIPE:
+                setData((byte) 0x7);
+                return;
+        }
     }
 
     @Override
