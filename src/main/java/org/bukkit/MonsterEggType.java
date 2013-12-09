51e4fb644b1441cb69293bc8ee22106f0f2ebd95
GJ <gjmcferrin@gmail.com>
2013-12-09 10:32:58 -0500

    Add support for new MonsterEgg types.
    Due to the way the new types were added by Mojang, all of the
    TexturedMaterial functions have been deprecated and replaced with
    enum-based alternatives. The previous functions will not work for
    getting or setting the three new MonsterEgg types.

---
+++ src/main/java/org/bukkit/MonsterEggType.java
@@ -0,0 +1,10 @@
+package org.bukkit;
+
+public enum MonsterEggType {
+    STONE,
+    COBBLESTONE,
+    STONE_BRICK,
+    MOSSY_STONE_BRICK,
+    CRACKED_STONE_BRICK,
+    CHISELED_STONE_BRICK;
+}
--- src/main/java/org/bukkit/material/MonsterEggs.java
+++ src/main/java/org/bukkit/material/MonsterEggs.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.MonsterEggType;
 
 /**
  * Represents the different types of monster eggs
@@ -19,6 +20,11 @@ public class MonsterEggs extends TexturedMaterial {
 
     public MonsterEggs() {
         super(Material.MONSTER_EGGS);
+    }
+
+    public MonsterEggs(MonsterEggType type) {
+        this();
+        setAppearance(type);
     }
 
     /**
@@ -55,9 +61,87 @@ public class MonsterEggs extends TexturedMaterial {
         super(type, data);
     }
 
+    /**
+     *
+     * @deprecated Does not work for all types
+     */
     @Override
+    @Deprecated
     public List<Material> getTextures() {
         return textures;
+    }
+
+    /**
+     *
+     * @deprecated Does not work for all types. Use {@link #getAppearance()} instead.
+     */
+    @Deprecated
+    public Material getMaterial() {
+        int n = getTextureIndex();
+        if (n > getTextures().size() - 1) {
+            n = 0;
+        }
+
+        return getTextures().get(n);
+    }
+
+    /**
+     * Sets the material this block is made of
+     *
+     * @param material
+     *            New material of this block
+     *
+     * @deprecated Does not work for all types. Use {@link #setAppearance(MonsterEggType)} instead.
+     */
+    @Deprecated
+    public void setMaterial(Material material) {
+        if (getTextures().contains(material)) {
+            setTextureIndex(getTextures().indexOf(material));
+        } else {
+            setTextureIndex(0x0);
+        }
+    }
+
+    public MonsterEggType getAppearance() {
+        switch (getData()) {
+            case 0x0:
+                return MonsterEggType.STONE;
+            case 0x1:
+                return MonsterEggType.COBBLESTONE;
+            case 0x2:
+                return MonsterEggType.STONE_BRICK;
+            case 0x3:
+                return MonsterEggType.MOSSY_STONE_BRICK;
+            case 0x4:
+                return MonsterEggType.CRACKED_STONE_BRICK;
+            case 0x5:
+                return MonsterEggType.CHISELED_STONE_BRICK;
+            default:
+                return null;
+        }
+    }
+
+    public void setAppearance(MonsterEggType type) {
+        switch (type) {
+            case STONE:
+                setData((byte) 0x0);
+                return;
+            case COBBLESTONE:
+                setData((byte) 0x1);
+                return;
+            case STONE_BRICK:
+                setData((byte) 0x2);
+                return;
+            case MOSSY_STONE_BRICK:
+                setData((byte) 0x3);
+                return;
+            case CRACKED_STONE_BRICK:
+                setData((byte) 0x4);
+                return;
+            case CHISELED_STONE_BRICK:
+                setData((byte) 0x5);
+                return;
+        }
     }
 
     @Override
